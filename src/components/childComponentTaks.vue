7cac2928039777abd81d0884577114f780f2810e
Jeevan Boyini <jeevan.boyini@healthcatalyst.com>
2023-04-23 23:52:49 +0530

    initial commit

---
+++ src/components/childComponentTaks.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul>
+        <li>
+            <div>{{ task.name }} --- {{ index }}</div>
+            <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+           <!--  <button @click="deleteTask(index)">X</button> -->
+        </li>
+    </ul>
+</template>
+
+
+
+<script>
+
+export default ({
+    props: ['task', 'index'],
+
+/*     methods: {
+        deleteTask(index) {
+            console.log(index)
+            this.tasks.splice(index, 1);
+            console.log(this.tasks);
+        }
+    }, */
+})
+
+</script>
+
+
+
+<style ></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page pdding>
+    <childComponent 
+    v-for="(task, index) in tasks" 
+    :key="task.id" 
+    :task="task"
+    :index="index">{{ index }}</childComponent>
+    <ul>
+
+    </ul>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import childComponentTaksVue from '../components/childComponentTaks.vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+
+
+  data() {
+    return {
+      tasks: [
+        {
+          id: 0,
+          name: 'Go to shop',
+          dueDate: '2019/05/1',
+          dueTime: '18:30'
+        },
+        {
+          id: 1,
+          name: 'Get bananas',
+          dueDate: '2019/05/1',
+          dueTime: '14:00'
+        },
+        {
+          id: 2,
+          name: 'Get apples',
+          dueDate: '2019/05/1',
+          dueTime: '132.001'
+        }
+      ]
+    }
+  },
+
+  components: {
+    'childComponent': childComponentTaksVue
+  }
+
+})
+</script>
